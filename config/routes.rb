0d989d3b6ce2e5bbc9df1b9319c12883772f642c
Chris Shaw <cshaw223@gmail.com>
2015-10-23 00:30:00 -0400

    Added the about page

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+ 
+ get 'about' => "pages#about"
+    #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
